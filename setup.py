2e2e9cca1110aaee71a63eb05e9d24a130d95e97
nemunemu <git@nemumu.com>
2014-05-24 11:49:44 +0900

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup( name = "nemunemuhello",
+	version = "0.3",
+	py_modules = ["nemunemuhello"],
+	license = "BSD License",
+	description = "This is my test modules",
+	author = "nemunemu",
+	author_email = "oss@nemumu.com",
+	url = "https://github.com/nemumu/pypi-helloworld",
+	keywords = ["package"],
+	
+)
